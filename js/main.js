f1e730e7f5a162e35f6483a72b1fc1a017bef28a
Agung Rilo Pambudi <56600401+AgungRilo@users.noreply.github.com>
2024-08-26 14:04:39 +0700

    WA direct

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
-jQuery(function($) {
+jQuery(function ($) {
 
-	
-	
+
+
 	// accordian
-	$('.accordion-toggle').on('click', function(){
-		$(this).closest('.panel-group').children().each(function(){
-		$(this).find('>.panel-heading').removeClass('active');
-		 });
+	$('.accordion-toggle').on('click', function () {
+		$(this).closest('.panel-group').children().each(function () {
+			$(this).find('>.panel-heading').removeClass('active');
+		});
 
-	 	$(this).closest('.panel-heading').toggleClass('active');
+		$(this).closest('.panel-heading').toggleClass('active');
 	});
 
 	//Initiat WOW JS
 	new WOW().init();
 
 	// portfolio filter
-	$(window).load(function(){'use strict';
+	$(window).load(function () {
+		'use strict';
 		var $portfolio_selectors = $('.portfolio-filter >li>a');
 		var $portfolio = $('.portfolio-items');
 		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
+			itemSelector: '.portfolio-item',
+			layoutMode: 'fitRows'
 		});
-		
-		$portfolio_selectors.on('click', function(){
+
+		$portfolio_selectors.on('click', function () {
 			$portfolio_selectors.removeClass('active');
 			$(this).addClass('active');
 			var selector = $(this).attr('data-filter');
@@ -31,44 +32,61 @@ jQuery(function($) {
 			return false;
 		});
 	});
-
+	function jsonToText(json) {
+		return `Saya ingin mendaftar ke kelas berikut biodata saya:\n\n` +
+			`Nama: ${json.name}\n` +
+			`Email: ${json.email}\n` +			
+			`Universitas: ${json.university}\n`+
+			`Subjek: ${json.subject}\n` +
+			`Pesan: ${json.message}\n` ;
+	}
 	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
+	var form = $('#main-contact-form');	
+	form.submit(function (event) {
+		let data = {
+			name: document.getElementById("name").value,
+			email: document.getElementById("email").value,
+			university: document.getElementById("university").value,
+			subject:document.getElementById("subject").value,
+			message:document.getElementById("message").value			
+		}		
 		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
 
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-			}
-		}).done(function(data){
-			form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
-		});
-	});
+		const formattedText = jsonToText(data);
+		const uriEncodedText = encodeURIComponent(formattedText);		
 
-	
-	
+		// Nomor telepon tujuan di WhatsApp
+		const phoneNumber = "628151654015"; // Ganti dengan nomor tujuan yang sesuai
+
+		// URL WhatsApp API dengan teks terenkode
+		const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${uriEncodedText}`;
+
+		// Redirect ke URL WhatsApp
+		window.open(whatsappUrl, '_blank');
+	}	
+	);
+
+
+
 
 	//Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
 });
 
 function resizeImage() {
-    var image = document.getElementById('responsiveImage');
-	console.log("image",window.innerWidth);
-    if (window.innerWidth <= 768) { // Resolusi untuk mobile
-      image.style.width = '90px';
-    } else { // Resolusi untuk web
-      image.style.width = '120px';
-    }
-  }
+	var image = document.getElementById('responsiveImage');
+	console.log("image", window.innerWidth);
+	if (window.innerWidth <= 768) { // Resolusi untuk mobile
+		image.style.width = '90px';
+	} else { // Resolusi untuk web
+		image.style.width = '120px';
+	}
+}
 
-  // Panggil fungsi resizeImage saat halaman dimuat
-  window.onload = resizeImage;
+// Panggil fungsi resizeImage saat halaman dimuat
+window.onload = resizeImage;
 
-  // Panggil fungsi resizeImage setiap kali ukuran jendela berubah
-  window.onresize = resizeImage;
+// Panggil fungsi resizeImage setiap kali ukuran jendela berubah
+window.onresize = resizeImage;
