30c74ff4f525eda33a61328b35492cc4098d0f08
Ikhlasul Amal <54785909+ikhlasul0507@users.noreply.github.com>
2024-10-09 14:19:23 +0700

    pudate msg

--- webhook.php
+++ webhook.php
@@ -169,7 +169,7 @@ function process_call_wa($conn, $handphone, $namalengkap)
             //send group chat
             $dataSend = "Halo, Saya " . $user['nama_lengkap'] . " 
 Admin Peradi, 
-Silahkan Gabung Group
+Silahkan Gabung Group Sosilisasi
 
 " . $linkGroupWaSosilisasi . ".
             
